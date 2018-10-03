93f5f553f81b5d455ab1768c3c7c4c6ae4b5c6d6
Fred Tarbox <fredtbx@Freds-MacBook-Pro.local>
2018-10-03 09:53:12 -0400

    Customize styles, add Google Font

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 import './App.css';
 import styled from 'styled-components';
 
+const CustomElement = styled.div`
+color: green
+`;
+
 class App extends Component {
   render() {
+    
     return (
       <div className="App">
-        Hello
+        <CustomElement>
+          Hello
+        </CustomElement>
       </div>
     );
   }

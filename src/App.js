577a2f97e3ba9d1fe5fdbe8406b7dceced8b9012
Fred Tarbox <fredtbx@Freds-MacBook-Pro.local>
2018-10-05 11:41:18 -0400

    Dashboard layout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 import styled from 'styled-components';
 import AppBar from './AppBar';
 import CoinList from './CoinList';
+import Search from './Search';
+import Dashboard from './Dashboard';
+import {ConfirmButton} from './Button';
+import _ from 'lodash';
+import fuzzy from 'fuzzy';
+
 const cc = require('cryptocompare');
+
 
 const AppLayout = styled.div`
 padding: 40px; 
@@ -12,29 +19,58 @@ padding: 40px;
 const Content = styled.div`
   
 `
+export const CenterDiv = styled.div`
+  display: grid;
+  justify-content: center;
+`
+
+const MAX_FAVORITES = 10;
+
 const checkFirstVisit = () => {
-  let cryptoDashData = localStorage.getItem('cryptoDash');
+  let cryptoDashData = JSON.parse(localStorage.getItem('cryptoDash'));
   if (!cryptoDashData) {
     return {
       firstVisit: true,
       page: 'settings'
     }
   }
-  return {}
+  return {
+    favorites: cryptoDashData.favorites
+  };
 }
 
 class App extends Component {
   state = {
-    page: 'settings',
+    page: 'dashboard',
+    //favorites: ['ETH', 'BTC', 'XMR', 'DOGE', 'EOS'],
     ...checkFirstVisit()
   } 
   componentDidMount = () => {
     this.fetchCoins();
+    this.fetchPrices();
   }
 
   fetchCoins = async() => {
     let coinList = (await cc.coinList()).Data;
     this.setState({ coinList });
+  }
+
+  fetchPrices = async () => {
+    let prices;
+    try {
+      prices = await this.prices();      
+    } catch(e) {      
+      this.setState({error: true});
+    }
+    this.setState({prices});
+  }
+
+  prices = () => {
+    let promises = [];    
+    this.state.favorites.forEach(sym => {
+      promises.push(cc.priceFull(sym, 'USD'));
+    })
+    return Promise.all(promises);
   }
   displayingDashboard = () => this.state.page === 'dashboard';
   displayingSettings = () => this.state.page === 'settings';
@@ -44,25 +80,71 @@ class App extends Component {
     }
   }
   confirmFavorites = () => {
-    localStorage.setItem('cryptoDash', 'test');
     this.setState({
       firstVisit: false,
-      page: 'dashboard'
-    })
+      page: 'dashboard',
+      prices: null
+    });
+    this.fetchPrices();
+    localStorage.setItem('cryptoDash', JSON.stringify({
+      favorites: this.state.favorites
+    }));
   }
   settingsContent = () => {
     return <div>
-      {this.firstVisitMessage()}
-      <div onClick={this.confirmFavorites}>
-        Confirm Favorites
-      </div>
-      <div>{CoinList.call(this)}</div>
+      {this.firstVisitMessage()}      
+      <div>
+      {CoinList.call(this, true)}
+      <CenterDiv>
+        <ConfirmButton onClick={this.confirmFavorites}>
+          Confirm Favorites
+        </ConfirmButton>
+      </CenterDiv>
+      {Search.call(this)}
+      {CoinList.call(this)}</div>
     </div>
   }
   loadingContent = () => {
     if(!this.state.coinList) {
       return <div>Loading Coins </div>
     }
+    if(!this.state.prices) {
+      return <div>Loading prices</div>
+    }
+  }
+
+  addCoinToFavorites = (key) => {
+    let favorites = [...this.state.favorites];
+    if (favorites.length < MAX_FAVORITES) {
+      favorites.push(key);
+      this.setState({favorites})
+    }
+  }
+
+  removeCoinFromFavorites = (key) => {
+    let favorites = [...this.state.favorites];
+    this.setState({favorites: _.pull(favorites, key)}); 
+  }
+
+  isInFavorites = (key) => _.includes(this.state.favorites, key);
+  handleFilter = _.debounce((inputValue) => {
+    // get all the coin symbols
+    let coinSymbols = Object.keys(this.state.coinList);
+    // Get all the coin names
+    let coinNames = coinSymbols.map(sym => this.state.coinList[sym].CoinName);
+    let allStringsToSearch = coinSymbols.concat(coinNames);
+    let fuzzyResults = fuzzy.filter(inputValue, allStringsToSearch, {}).map(result => result.string);
+    
+    let filteredCoins = _.pickBy(this.state.coinList, (result, symKey) => {
+      let coinName = result.CoinName;
+      // If our fuzzy results contain this symbol OR the coinName, include it (return true).
+      return _.includes(fuzzyResults, symKey) || _.includes(fuzzyResults, coinName);
+    });
+    this.setState({ filteredCoins });
+  }, 500)
+  filterCoins = e => {
+    let inputValue = _.get(e, 'target.value');
+    this.handleFilter(inputValue);
   }
   render() {    
     return (
@@ -70,7 +152,8 @@ class App extends Component {
       {AppBar.call(this)}
       {this.loadingContent() || <Content>
           {this.displayingSettings() && this.settingsContent()}
-      </Content>}
+          {this.displayingDashboard() && Dashboard.call(this)}
+          </Content>}
       </AppLayout>
     );
   }
--- src/CoinList.js
+++ src/CoinList.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import styled from 'styled-components';
-import {subtleBoxShadow, greenBoxShadow, lightBlueBackground} from './Style'
+import styled, {css} from 'styled-components';
+import {subtleBoxShadow, greenBoxShadow, redBoxShadow, lightBlueBackground} from './Style';
+import _ from 'lodash';
 
-const CoinGrid = styled.div`
+export const CoinGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+  ${props => props.count && css`
+    grid-template-columns: repeat(10, 1fr)
+  `}
   grid-gap: 15px;
   margin-top: 40px;
 `
-const CoinTile = styled.div`
+export const CoinTile = styled.div`
   ${subtleBoxShadow}
   ${lightBlueBackground}
   padding: 10px;
@@ -16,24 +20,49 @@ const CoinTile = styled.div`
     cursor: pointer;
     ${greenBoxShadow}
   }
+  ${props => props.favorite && css`
+    &:hover{
+      cursor: pointer;
+      ${redBoxShadow}
+  ` }
+  }
+  ${props => props.chosen && !props.favorite && css`
+    pointer-events: none;
+    opacity: 0.4;
+  `}
 `
-const CoinHeaderGrid = styled.div`
+export const CoinHeaderGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
 `
-const CoinSymbol = styled.div`
+export const CoinSymbol = styled.div`
   justify-self: right;
 `
+const DeleteIcon = styled.div`
+  justify-self: right;
+  display: none; 
+  ${CoinTile}:hover & {
+    display: block;
+    color: red;
+  }
+`
 
-export default function() {
+export default function(favorites=false) {
+  let coinKeys = favorites ? 
+    this.state.favorites : 
+    ((this.state.filteredCoins && Object.keys(this.state.filteredCoins)) || Object.keys(this.state.coinList).slice(0, 100));
   return <CoinGrid> 
-    {Object.keys(this.state.coinList).slice(0, 100).map(coin => 
-      <CoinTile>
+    {coinKeys.map(coinKey => 
+      <CoinTile chosen={this.isInFavorites(coinKey)} favorite={favorites} onClick={
+        favorites ? ()=>{this.removeCoinFromFavorites(coinKey)} : ()=>{this.addCoinToFavorites(coinKey)}}>
         <CoinHeaderGrid>
-          <div>{this.state.coinList[coin].CoinName}</div>
-          <CoinSymbol>{this.state.coinList[coin].Symbol}</CoinSymbol>
-          {<img style={{height: '50px'}} src={`http://cryptocompare.com/${this.state.coinList[coin].ImageUrl}`} />}
+          <div>{this.state.coinList[coinKey].CoinName}</div>
+          {favorites ? 
+            <DeleteIcon>X</DeleteIcon> : 
+              <CoinSymbol>{this.state.coinList[coinKey].Symbol}</CoinSymbol>}          
         </CoinHeaderGrid>
-      </CoinTile>)}
+        {<img style={{height: '50px'}} src={`http://cryptocompare.com/${this.state.coinList[coinKey].ImageUrl}`} />}
+      </CoinTile>
+      )}
   </CoinGrid>
 }
